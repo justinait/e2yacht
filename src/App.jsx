d09c4d4fa9a184a00067ef236b1c51656fcbc229
justinait <justiiturraspe@gmail.com>
2024-08-19 19:23:54 -0300

    hero home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import '@fontsource-variable/libre-franklin';
-import Navbar from './Navbar/Navbar';
+import CheckScroll from './CheckScroll';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
 
 function App() {
 
   return (
-    <>
-      <Navbar/>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <CheckScroll/>
+      <Routes className='appRoutes'>
+
+        <Route path='/' element={< Home />} />
+        
+        {/* <Route path='/about' element={< About />} />
+        <Route path='/news' element={< News />} />
+        <Route path='/atelier' element={< Atelier />} />
+        <Route path='/contact' element={< Contact />} />
+
+        <Route path='/:category' element={< GalleryContainer />} />
+        <Route path='/prensa/:id' element={< NewsDetail />} /> */}
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
