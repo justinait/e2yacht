784a6fc504613a8ed14c829c580552d200d93dce
justinait <justiiturraspe@gmail.com>
2024-08-20 02:53:39 -0300

    navbar links

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,84 +6,84 @@ import CloseIcon from '/icons/closeMenu.png'
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-    const [openMenu, setOpenMenu] = useState(false);
-    const [selectedCategory, setSelectedCategory] = useState('')
+  const [openMenu, setOpenMenu] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState('')
 
-    const sections = [
-      { name: 'Services', id: 'services', className: '' },
-      { name: 'Our Crew', id: 'crew', className: ''},
-      { name: 'Where we are', id: 'where', className: ''},
-      { name: 'Contact', id: 'contact', className: ''}
-    ]
-    const windowWidth = window.innerWidth;
-    const [desktop, setDesktop] = (windowWidth <= 1200) ? useState(false): useState(true)
+  const sections = [
+    { name: 'Services', id: 'services', className: '' },
+    { name: 'Our Crew', id: 'crew', className: ''},
+    { name: 'Where we are', id: 'where', className: ''},
+    { name: 'Contact', id: 'contact', className: ''}
+  ]
+  const windowWidth = window.innerWidth;
+  const [desktop, setDesktop] = (windowWidth <= 1200) ? useState(false): useState(true)
 
-    const handleClose = (id) => {
-      if(!desktop){
-        setOpenMenu(false);
-      }
-      if(id){
-        setSelectedCategory(id);
-      }
-        
+  const handleClose = (id) => {
+    if(!desktop){
+      setOpenMenu(false);
     }
+    if(id){
+      setSelectedCategory(id);
+    }
+      
+  }
 
-    useEffect(() => {
-        const handleResize = () => {
-            setDesktop(window.innerWidth > 1200);
-        };
+  useEffect(() => {
+    const handleResize = () => {
+      setDesktop(window.innerWidth > 1200);
+    };
 
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
-    const handleOpen = () => {
-        if(!desktop){
-            console.log('hola');
-          if(openMenu)
-            return setOpenMenu(false);
-          else 
-            return setOpenMenu(true);
-        } else {
-          setOpenMenu(true)
+  const handleOpen = () => {
+    if(!desktop){
+        console.log('hola');
+      if(openMenu)
+        return setOpenMenu(false);
+      else 
+        return setOpenMenu(true);
+    } else {
+      setOpenMenu(true)
+    }
+  }
+  return (
+    <div className='navbarContainer'>
+      <div className='navbar'>
+        <img src={logo} alt="E 2 Yacht Services" className='logoNavbar'/>
+        {
+          !desktop && (
+          openMenu ?
+          <img src={CloseIcon}  onClick={handleClose} className='closeMenuIcon' alt="Menu" />:
+          <img src={menu}  onClick={handleOpen} className='burgerMenuIcon' alt="X" />)
         }
-    }
-    return (
-        <div className='navbarContainer'>
-            <div className='navbar'>
-                <img src={logo} alt="E 2 Yacht Services" className='logoNavbar'/>
-                {
-                !desktop && (
-                openMenu ?
-                <img src={CloseIcon}  onClick={handleClose} className='closeMenuIcon' alt="Menu" />:
-                <img src={menu}  onClick={handleOpen} className='burgerMenuIcon' alt="X" />)
-                }
-            </div>
+      </div>
 
-            {(openMenu || desktop) &&
-            <div className='dropdownHeader'>
-              
-              <div className='dropdownItemsContainer'>
-                
-                {sections.map((e, i)=> {
-                  return (
-                    <p
-                      key={e.id} 
-                      to={`/${e.id}`} 
-                      onClick={()=>handleClose(e.id)} 
-                      className={selectedCategory == e.id? 'dropdownItems activeNavbar': 'dropdownItems'}>
-                      {e.name}
-                    </p>)
-                })}
-                
-              </div>
-            </div>
-          }
+      {(openMenu || desktop) &&
+        <div className='dropdownHeader'>
+          
+          <div className='dropdownItemsContainer'>
+            
+            {sections.map((e, i)=> {
+              return (
+                <Link
+                  key={e.id} 
+                  to={`/${e.id}`} 
+                  onClick={()=>handleClose(e.id)} 
+                  className={selectedCategory == e.id? 'dropdownItems activeNavbar': 'dropdownItems'}>
+                  {e.name}
+                </Link>
+              )
+            })}
+            
+          </div>
         </div>
-        
-    )
+      }
+    </div>
+  )
 }
 
 export default Navbar
