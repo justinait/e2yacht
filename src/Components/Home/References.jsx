214652f6944fd0cdd047898bfda0e17203b804c2
justinait <justiiturraspe@gmail.com>
2024-08-20 01:54:00 -0300

    roots

--- src/Components/Home/References.jsx
+++ src/Components/Home/References.jsx
@@ -2,12 +2,30 @@ import React from 'react'
 import './References.css'
 
 function References() {
+ 
+  const references = [
+    { name: 'Harold Ziegler, MV Ziggy, 2009 Hatteras 72’', text: '“Elizabeth, I wanted to thank you and let you know that I recommend you as a delivery captain. You completed my delivery from the Bahamas to Michigan in a timely fashion. There were times that you ran the boat all night in order to get it back to Michigan and meet my scheduled trip. I appreciate your dedication and commitment to making things happen.”', className: '' },
+    { name: 'yacht deliveries', text: '/images/deliveriesHero.jpg', className: ''},
+    { name: 'PRIVATE INSTRUCTIONS', text: '/images/instructionHero.jpg', className: ''},
+    { name: 'marine survey', text: '/images/marineSurveyHero.jpg', className: ''},
+    { name: 'marine asset recovery', text: '/images/maritimeRecovery.jpg', className: ''}
+  ]
   return (
-    <div>
+    <div className='homeContainers whereHomeContainer'>
         
-        <h5 className='servicesTitleHome'>References</h5>
-        <h2 className='servicesSubtitleHome'>Create new experiences by working with us</h2>
+      <h5 className='titlesHome'>References</h5>
+      <h2 className='subtitlesHome'>Create new experiences by working with us</h2>
 
+      {
+        references.map((e, i)=> {
+          return(
+            <div className='referenceCard'>
+              <p className='referenceCardName'>{e.name}</p>
+              <p className='referenceCardText'>{e.text}</p>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
--- src/Components/Home/Where.jsx
+++ src/Components/Home/Where.jsx
@@ -4,12 +4,11 @@ import photo from '/images/servingBanderasHome.jpg'
 
 function Where() {
   return (
-    <div className='WhereHomeContainer'>
-        <h5 className='servicesTitleHome'>where we are</h5>
-        <h2 className='servicesSubtitleHome'>You can find our offices in Mexico, but we also work around the world.</h2>
+    <div className='homeContainers whereHomeContainer'>
+      <h5 className='titlesHome'>where we are</h5>
+      <h2 className='subtitlesHome'>You can find our offices in Mexico, but we also work around the world.</h2>
 
-        <img src={photo} alt="" className='WhereImageHome'/>
-        <p className='button'>SEE MORE</p>
+      <img src={photo} alt="" className='ourCrewImageHome'/>
     </div>
   )
 }
