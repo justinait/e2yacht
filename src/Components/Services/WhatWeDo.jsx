5ba97dcac826253f46b89ff02aad829c9cb173e1
justinait <justiiturraspe@gmail.com>
2024-08-20 18:25:29 -0300

    wwd

--- src/Components/Services/WhatWeDo.jsx
+++ src/Components/Services/WhatWeDo.jsx
@@ -14,6 +14,20 @@ function WhatWeDo() {
         <img src={menu} alt="" className='menuServices' />
         <h5 className='titlesHome servicesTitles'>Our Services</h5>
       </div>
+
+      <div className='whatWeDoTextContainer'>
+        <p>E2 Yacht Services provides the highest quality of professional motor and sailing yacht services throughout 
+          North America, Mexico, Central and South America, the Caribbean, and beyond. We offer the following for yacht 
+          and boat owners, agents and brokers:
+        </p>
+        <ul>
+          <li>Professional and careful motor and sailing yacht delivery insurable year round world wide</li>
+          <li>Maintenance while you are away from your yacht, weekly washing to project management and system upgrades</li>
+          <li>Complete captain and crewing services, and specialties including , fishing guides, diver masters, surf instruction, etc.</li>
+          <li>Complete yacht and project management.</li>
+          <li>Hands-on, personal training and instruction</li>
+        </ul>
+      </div>
     </div>
   )
 }
