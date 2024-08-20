a786615fb646f67d0772a8ab773d49fdee1b4292
justinait <justiiturraspe@gmail.com>
2024-08-20 16:59:53 -0300

    contact form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Contact.css'
 import location from '/icons/location.png'
 import phone from '/icons/telefono.png'
 import mail from '/icons/Correo.png'
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const whatsappMessage = `Hola, mi nombre es ${name} y mi consulta es: ${message}`;
+    const whatsappUrl = `https://api.whatsapp.com/send?phone=+5213221303534&text=${encodeURIComponent(whatsappMessage)}`;
+
+    window.open(whatsappUrl, '_blank');
+  };
   return (
     <div>
       <div className='contactHero heroImages'>
@@ -44,6 +56,54 @@ function Contact() {
       <div className='contactFormBigContainer'>
         <h2 className='contactBigSubtitle'>We’d love to hear from you</h2>
 
+        <form onSubmit={handleSubmit}>
+          <div className='inputContainer'>
+            <label htmlFor="name" className={`floatingLabel ${name && 'floatingLabelActive'}`}>Name*</label>
+            <input 
+              type="text" 
+              id="name" 
+              value={name} 
+              onChange={(e) => setName(e.target.value)} 
+              required 
+              // placeholder='Name*' 
+              className='inputContact'
+            />
+          </div>
+          <div className='inputContainer'>
+            <label htmlFor="email" className={`floatingLabel ${email && 'floatingLabelActive'}`}>Email*</label>
+            <input 
+              type="text" 
+              id="email" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+              required 
+              className='inputContact'
+            />
+          </div>
+          <div className='inputContainer'>
+            <label htmlFor="subject" className={`floatingLabel ${subject && 'floatingLabelActive'}`}>Subject*</label>
+            <input 
+              type="text" 
+              id="subject" 
+              value={subject} 
+              onChange={(e) => setSubject(e.target.value)} 
+              required 
+              className='inputContact'
+            />
+          </div>
+          <div className='inputContainer'>
+            <label htmlFor="message" className={`floatingLabel ${message && 'floatingLabelActive'}`}>Your message*</label>
+            <input 
+              id="message" 
+              value={message} 
+              onChange={(e) => setMessage(e.target.value)} 
+              required 
+              // placeholder='Your message*' 
+              className='inputContact'
+            />
+          </div>
+        <button className='button formButton'>Send Message</button>
+        </form>
       </div>
     </div>
   )
