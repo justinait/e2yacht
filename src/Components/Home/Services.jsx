5e41faee1473b68f222760f803ac8dd4cbd4f413
justinait <justiiturraspe@gmail.com>
2024-08-20 17:28:28 -0300

    links home

--- src/Components/Home/Services.jsx
+++ src/Components/Home/Services.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Services.css'
+import { Link } from 'react-router-dom'
 
 function Services() {
     
     const services = [
         { name: 'Yacht management', id: '/management', image: '/images/whatWeDoHero.jpg', className: '' },
         { name: 'yacht deliveries', id: '/deliveries', image: '/images/deliveriesHero.jpg', className: ''},
-        { name: 'PRIVATE INSTRUCTIONS', id: '/instructions', image: '/images/instructionHero.jpg', className: ''},
+        { name: 'PRIVATE INSTRUCTIONS', id: '/instruction', image: '/images/instructionHero.jpg', className: ''},
         { name: 'marine survey', id: '/marinesurvey', image: '/images/marineSurveyHero.jpg', className: ''},
-        { name: 'captain and crew services', id: 'crew', image: '/images/captainAndCrewServices.jpg', className: ''},
+        { name: 'captain and crew services', id: '/captainandcrew', image: '/images/captainAndCrewServices.jpg', className: ''},
         { name: 'marine asset recovery', id: '/marinerecovery', image: '/images/maritimeRecovery.jpg', className: ''}
     ]
 
@@ -20,10 +21,10 @@ function Services() {
             {
                 services.map((e, i)=> {
                     return(
-                        <div className='servicesCardHome' key={i}>
+                        <Link to={e.id} className='servicesCardHome' key={i}>
                             <img src={e.image} alt={e.name} />
                             <p className='homeServiceName'>{e.name}</p>
-                        </div>
+                        </Link>
                     )
                 })
             }
