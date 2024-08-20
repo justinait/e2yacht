13302544764586baf2f8d207bb568ede511951e4
justinait <justiiturraspe@gmail.com>
2024-08-20 02:48:19 -0300

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,11 +5,14 @@ import linkedinHover from '/icons/linkedinHover.png'
 import linkedin from '/icons/linkedin.png'
 import instagram from '/icons/instagram.png'
 import instagramHover from '/icons/instagramHover.png'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
     <div className='footerContainer homeContainers'>
+        
         <h3 className='footerTitle'>The benefits of a full time crew without the expense yacht management, delivery and staffing.</h3>
+        
         <img src={logo} alt="E2 yacht services" className='logoFooter' />
         
         <div className='contactFooter'>
@@ -22,6 +25,26 @@ function Footer() {
                     <img src={instagram} alt="ig" />
                 </div>
             </div>
+        </div>
+
+        <div className='indice'>
+            <section>
+                <h6>Home</h6>
+                <Link className='linksHomeFooterIndice' to='crew'>Our Crew </Link>
+                <Link className='linksHomeFooterIndice' to='contact'>Contact Us </Link>
+                <Link className='linksHomeFooterIndice' to='services'>Services </Link>
+            </section>
+            <section>
+                <h6>Location</h6>
+                <p className='locationFooter'>Located in Beautiful Paradise Village Marina, Nuevo Vallarta, Nayarit, 63732, Mexico</p>
+            </section>
+            <section>
+                <h6>Contact</h6>
+                <p className='locationFooter'>info@e2yachtservices.com</p>
+                <p className='locationFooter'>Office: +52 322 297 4065</p>
+                <p className='locationFooter'>WhatsApp: +52 322-134-7397</p>
+                <p className='locationFooter'>USA: #1-714-412-6180</p>
+            </section>
         </div>
 
 
--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -4,17 +4,17 @@ import './Hero.css'
 function Hero() {
   return (
     <div className='heroHome'>
-        <h1>Professional Yacht Services</h1>
-        <h4 className='heroSubtitle'>Your resource based in Puerto Vallarta and Pacific Mexico including Baja California and the Sea of Cortez</h4>
-        <div className='separator'></div>
-        <p className='heroDescription'>Operated by <strong>Captain Elizabeth Shanahan </strong> <br />
-          and <strong>Captain Eugenie Russel </strong> 
-        </p>
+      <h1>Professional Yacht Services</h1>
+      <h4 className='heroSubtitle'>Your resource based in Puerto Vallarta and Pacific Mexico including Baja California and the Sea of Cortez</h4>
+      <div className='separator'></div>
+      <p className='heroDescription'>Operated by <strong>Captain Elizabeth Shanahan </strong> <br />
+        and <strong>Captain Eugenie Russel </strong> 
+      </p>
 
-        <div className='buttonsHero'>
-          <p className='button learnMore'>Learn More</p>
-          <p className='button'>Contact us</p>
-        </div>
+      <div className='buttonsHero'>
+        <p className='button learnMore'>Learn More</p>
+        <p className='button'>Contact us</p>
+      </div>
     </div>
   )
 }
