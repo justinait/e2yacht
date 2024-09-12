1bc5b2b549f99eaf6f9a92967006b6f2d71cc4b4
justinait <justiiturraspe@gmail.com>
2024-09-12 14:23:16 -0300

    pc grande hero

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -12,7 +12,7 @@ function Hero() {
   return (
     <div className='heroHome heroImages '>
       <h1>Professional Yacht Services</h1>
-      <h4 className='heroSubtitle'>Your resource based in Puerto Vallarta and Pacific Mexico including Baja California and the Sea of Cortez</h4>
+      <h4 className='heroSubtitle'>Your resource based in Puerto Vallarta and Pacific Mexico <br className='onlyForDesktop'/> including Baja California and the Sea of Cortez</h4>
       <div className='separator'></div>
       <p className='heroDescription'>Operated by <strong>Captain Elizabeth Shanahan </strong> <br className='notForDesktop' />
         and <strong>Captain Eugenie Russel </strong> 
