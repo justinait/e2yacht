850e6eba9915e0484b79e592f0efb901d6b05bcb
justinait <justiiturraspe@gmail.com>
2024-09-16 15:03:50 -0300

    correct text

--- src/Components/Home/Services.jsx
+++ src/Components/Home/Services.jsx
@@ -8,9 +8,9 @@ function Services() {
         { name: 'Yacht management', id: '/management', image: '/images/whatWeDoHero.jpg', text:'We maintain and manage yachts to the highest standards, allowing owners to enjoy worry-free time onboard, knowing every detail is in good hands.', className: '' },
         { name: 'yacht deliveries', id: '/deliveries', image: '/images/deliveriesHero.jpg', text:'Delivering boats worldwide, we specialize in the West Coast of North America, from Alaska to Mexico and Central America.', className: ''},
         { name: 'PRIVATE INSTRUCTIONS', id: '/instruction', image: '/images/instructionHero.jpg', text:'With decades of experience, we\'ve trained hundreds of seamen, sharing our passion for the open water. Our hands-on, personalized approach emphasizes safety above all else.', className: ''},
-        { name: 'marine survey', id: '/marinesurvey', image: '/images/marineSurveyHero.jpg', text:'Delivering boats worldwide, we specialize in the West Coast of North America, from Alaska to Mexico and Central America.', className: ''},
+        { name: 'marine survey', id: '/marinesurvey', image: '/images/marineSurveyHero.jpg', text:'We will provide you with a detailed, internationally accepted report of the condition and fair market value of your vessel.', className: ''},
         { name: 'captain and crew services', id: '/captainandcrew', image: '/images/captainAndCrewServices.jpg', text:'We provide temporary and on call captain and crew services. All of our staff is licensed.', className: ''},
-        { name: 'marine asset recovery', id: '/maritimerecovery', image: '/images/maritimeRecovery.jpg', text:'E2 Yacht Services can skip trace and recover boats and yachts of any size. We can operate on short notice and offer fast delivery.', className: ''}
+        { name: 'marine asset recovery', id: '/maritimerecovery', image: '/images/maritimeRecovery.jpg', text:'E2 Yacht Services can skip trace and recover boats and yachts of any size. We can operate on short notice and offer fast delivery.', className: ''}
     ]
 
   return (
