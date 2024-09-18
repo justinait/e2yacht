6b74c6a6bb38bbb26d117e0cdb5d5ec813927cad
justinait <justiiturraspe@gmail.com>
2024-09-18 15:40:01 -0300

    links footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,20 +10,20 @@ import { Link } from 'react-router-dom'
 function Footer() {
   return (
     <div className='footerContainer homeContainers'>
-        
-        <h3 className='footerTitle'>The benefits of a full time crew without the <br className='tablet1000'/> expense yacht management, delivery and staffing.</h3>
-        
-        <img src={logo} alt="E2 yacht services" className='logoFooter' />
+        <div>
+            <h3 className='footerTitle'>The benefits of a full time crew without the <br className='tablet1000'/> expense yacht management, delivery and staffing.</h3>
+            <img src={logo} alt="E2 yacht services" className='logoFooter' />
+        </div>        
         
         <div className='contactFooter'>
-            <p className='button learnMore contactButtonHover'>Contact Us</p>
+            <Link to='/contact' className='button learnMore'>Contact Us</Link>
             <div className='socialMedia'>
-                <div className='socialMediaCircle'>
+                <Link to='https://www.instagram.com/e2yachtservices/' className='socialMediaCircle'>
                     <img src={linkedin} alt="in" />
-                </div>
-                <div className='socialMediaCircle'>
+                </Link>
+                <Link to='https://www.instagram.com/e2yachtservices/' target='_blank' className='socialMediaCircle'>
                     <img src={instagram} alt="ig" />
-                </div>
+                </Link>
             </div>
         </div>
 
@@ -32,7 +32,7 @@ function Footer() {
                 <h6>Home</h6>
                 <Link className='linksHomeFooterIndice' to='crew'>Our Crew </Link>
                 <Link className='linksHomeFooterIndice' to='contact'>Contact Us </Link>
-                <Link className='linksHomeFooterIndice' to='services'>Services </Link>
+                <Link className='linksHomeFooterIndice' to='whatwedo'>Services </Link>
             </section>
             <section>
                 <h6>Location</h6>
@@ -40,10 +40,10 @@ function Footer() {
             </section>
             <section className='contactSectionFooter'>
                 <h6>Contact</h6>
-                <p className='locationFooter underline marginbottom'>info@e2yachtservices.com</p>
+                <a href="mailto:info@e2yachtservices.com" className='locationFooter underline marginbottom' target='_blank'>info@e2yachtservices.com</a>
                 {/* <br /> */}
                 <p className='locationFooter'>Office: +52 322 297 4065</p>
-                <p className='locationFooter underline'>WhatsApp: +52 322-134-7397</p>
+                <a href='https://api.whatsapp.com/send/?phone=523221347397&text&type=phone_number&app_absent=0' target='_blank' className='locationFooter underline'>WhatsApp: +52 322-134-7397</a>
                 <p className='locationFooter'>USA: #1-714-412-6180</p>
             </section>
         </div>
