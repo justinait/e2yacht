674e6b560a392551d1667ba0e14504d955c5041c
justinait <justiiturraspe@gmail.com>
2024-09-25 18:41:04 -0300

    services home traduccion

--- src/Components/Home/Services.jsx
+++ src/Components/Home/Services.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import './Services.css'
 import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next';
 
 function Services() {
-    
-    const services = [
-        { name: 'Yacht management', id: '/management', image: '/images/whatWeDoHero.jpg', text:'We maintain and manage yachts to the highest standards, allowing owners to enjoy worry-free time onboard, knowing every detail is in good hands.', className: '' },
-        { name: 'yacht deliveries', id: '/deliveries', image: '/images/deliveriesHero.jpg', text:'Delivering boats worldwide, we specialize in the West Coast of North America, from Alaska to Mexico and Central America.', className: ''},
-        { name: 'PRIVATE INSTRUCTIONS', id: '/instruction', image: '/images/instructionHero.jpg', text:'With decades of experience, we\'ve trained hundreds of seamen, sharing our passion for the open water. Our hands-on, personalized approach emphasizes safety above all else.', className: ''},
-        { name: 'marine survey', id: '/marinesurvey', image: '/images/marineSurveyHero.jpg', text:'We will provide you with a detailed, internationally accepted report of the condition and fair market value of your vessel.', className: ''},
-        { name: 'captain and crew services', id: '/captainandcrew', image: '/images/captainAndCrewServices.jpg', text:'We provide temporary and on call captain and crew services. All of our staff is licensed.', className: ''},
-        { name: 'marine asset recovery', id: '/maritimerecovery', image: '/images/maritimeRecovery.jpg', text:'E2 Yacht Services can skip trace and recover boats and yachts of any size. We can operate on short notice and offer fast delivery.', className: ''}
-    ]
+  const { t } = useTranslation();
+
+  const services = [
+    { name: t('services.yachtManagement.name'), id: '/management', image: '/images/whatWeDoHero.jpg', text: t('services.yachtManagement.text'), className: '' },
+    { name: t('services.yachtDeliveries.name'), id: '/deliveries', image: '/images/deliveriesHero.jpg', text: t('services.yachtDeliveries.text'), className: '' },
+    { name: t('services.privateInstructions.name'), id: '/instruction', image: '/images/instructionHero.jpg', text: t('services.privateInstructions.text'), className: '' },
+    { name: t('services.marineSurvey.name'), id: '/marinesurvey', image: '/images/marineSurveyHero.jpg', text: t('services.marineSurvey.text'), className: '' },
+    { name: t('services.captainAndCrewServices.name'), id: '/captainandcrew', image: '/images/captainAndCrewServices.jpg', text: t('services.captainAndCrewServices.text'), className: '' },
+    { name: t('services.marineAssetRecovery.name'), id: '/maritimerecovery', image: '/images/maritimeRecovery.jpg', text: t('services.marineAssetRecovery.text'), className: '' }
+  ];
 
   return (
     <div className='homeContainers servicesHomeContainer'>
-        <h5 className='titlesHome'>Services</h5>
+        <h5 className='titlesHome'>{t('services.title')}</h5>
         <h2 className='subtitlesHome'>E² Yacht Services</h2>
         <div className='servicesCardContainer'>
             {
