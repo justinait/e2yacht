05f590421ed8bf71abe0383c09fc018bbb858aac
justinait <justiiturraspe@gmail.com>
2024-09-25 20:31:39 -0300

    our crew traduccion

--- src/Components/OurCrew/OurCrew.jsx
+++ src/Components/OurCrew/OurCrew.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './OurCrew.css'
+import { useTranslation } from 'react-i18next';
 
 function OurCrew() {
+  const { t } = useTranslation();
   
   const crew = [
-    { name: 'Elizabeth Shanahan', text: 'Captain, 1600 T USCG Master, SA', image: '/images/ourCrewElizabeth.jpg', className: 'leftCrew' },
-    { name: 'Eugenie Russel', text: 'Captain, 1600 T USCG Master, SA', image: '/images/ourCrewEugene.jpg', className: 'rightCrew'},
-    { name: 'José Alejandro Cano', text: 'On the dock', image: '/images/ourCrewCano.jpg', className: 'leftCrew'},
-    { name: 'Odi Gutierrez', text: 'Office Manager', image: '/images/OdiCrew.jpeg', className: 'rightCrew'},
-    { name: 'Miguel Sánchez Rodríguez', text: 'On the dock', image: '/images/ourCrewCano.jpg', className: ''},
+    { name: 'Elizabeth Shanahan', text: t('ourCrew.captainText'), image: '/images/ourCrewElizabeth.jpg', className: 'leftCrew' },
+    { name: 'Eugenie Russel', text: t('ourCrew.captainText'), image: '/images/ourCrewEugene.jpg', className: 'rightCrew'},
+    { name: 'José Alejandro Cano', text: t('ourCrew.joseText'), image: '/images/ourCrewCano.jpg', className: 'leftCrew'},
+    { name: 'Odi Gutierrez', text: t('ourCrew.odiText'), image: '/images/OdiCrew.jpeg', className: 'rightCrew'},
+    { name: 'Miguel Sánchez Rodríguez', text: t('ourCrew.miguelText'), image: '/images/ourCrewCano.jpg', className: ''},
   ]
 
   return (
@@ -16,28 +18,24 @@ function OurCrew() {
 
       <div className='ourCrewHero heroImages'>
         <br />
-        <h3>Our Crew</h3>
+        <h3>{t('ourCrew.header')}</h3>
       </div>
 
       <div className='crewContainerCenter'>
         <div className='ourCrewSectionFirstDiv'>
           <div className='ourCrewInformation'>
-            <h2 className='subtitlesHome ourCrewH2'>The benefits of a full time crew without the expense yacht management, delivery and staffing.</h2>
+            <h2 className='subtitlesHome ourCrewH2'>{t('ourCrew.benefits')}</h2>
             <p className='descriptionCrew'>
-              Under the direction of United States Coast Guard Licensed Masters Elizabeth Shanahan and Eugenie Russell, 
-              our experienced team of marine professionals offer dedicated and personalized service, no matter the size or type of assignment. 
-              We have carefully and successfully captained sailing and motor yachts up and down the West Coast, including Mexico and Alaska. 
+              {t('ourCrew.description1')}
               <br className='notForDesktop' /><br className='notForDesktop' />
-               We also serve the East Coast, South America through the Caribbean, the Eastern Seaboard, through the Erie Canal, to the Great lakes 
-              and St. Lawrence Seaway, the South Pacific , Europe, and the Mediterranean. Our captains are available from 100T to unlimited, 
-              sailing and motor yachts as well as power boats.
+              {t('ourCrew.description2')}
             </p>
           </div>
           
           <div className='ourCrewBrujula heroImages'></div>
         </div>
         
-        <h2 className='subtitlesHome ourCrewH2'>Our Crew</h2>
+        <h2 className='subtitlesHome ourCrewH2'>{t('ourCrew.header')}</h2>
         <div className='crewDataContainer'>
           {
             crew.map((e, i)=> {
