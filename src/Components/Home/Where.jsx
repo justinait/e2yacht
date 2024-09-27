e5497d0e052cf68e942a7f937e25f87427def8b5
justinait <justiiturraspe@gmail.com>
2024-09-27 10:43:37 -0300

    where we are home

--- src/Components/Home/Where.jsx
+++ src/Components/Home/Where.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import './Where.css'
 import photo from '/images/servingBanderasHome.jpg'
 import Maps from '../Maps/Maps'
+import { useTranslation } from 'react-i18next';
 
 function Where() {
+  const { t } = useTranslation();
   return (
     <div className='homeContainers whereHomeContainer'>
       <div className='whereHomeTitles'>
-        <h5 className='titlesHome'>where we are</h5>
-        <h2 className='subtitlesHome'>You can find our offices in Mexico, <br className='onlyForDesktop'/> but we also work around the world.</h2>
+        <h5 className='titlesHome'>{t('contact.whereTitle')}</h5>
+        <h2 className='subtitlesHome'>{t('contact.youCanFindUs1')}
+          <br className='onlyForDesktop'/>{t('contact.youCanFindUs2')} </h2>
       </div>
 
       <Maps/>
